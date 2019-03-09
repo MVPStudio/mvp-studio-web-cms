a6af045f7c8cb7273a0e4ce7a50d7170f07281e5
Greg McKelvey <mckelveygreg@gmail.com>
2019-03-08 16:53:07 -0800

    refactor formHandler, errors crash server :(

--- server/formHandler.js
+++ server/formHandler.js
@@ -8,41 +8,33 @@ const {
   RECAPTCHA_SECRET,
 } = process.env;
 
-const saveContact = async data =>
-  new Promise((resolve, reject) => {
-    Airtable.configure({
-      apiKey: GATSBY_AIRTABLE_API_KEY,
-    });
-    const base = Airtable.base(AIRTABLE_BASE_ID);
-
-    // formName directs data to correct base
-    const { formName } = data;
-    // discard unnecessary form data
-    delete data.formName;
-    delete data.recaptcha;
-    // send form data
-    base(formName).create(data, err => {
-      if (err) return reject(err);
-      resolve();
-    });
+const saveContact = async data => {
+  Airtable.configure({
+    apiKey: GATSBY_AIRTABLE_API_KEY,
   });
-
-const verifyRecaptcha = async recaptcha =>
-  new Promise(async (resolve, reject) => {
-    const secret = RECAPTCHA_SECRET;
-    const response = await axios
-      .post(
-        `https://www.google.com/recaptcha/api/siteverify?secret=${secret}&response=${recaptcha}`,
-        {},
-      )
-      .then(res => res)
-      .catch(error => {
-        console.log(error);
-        reject(error);
-      });
-    if (!response.data.success) return reject(response.data['error-codes']);
-    resolve();
+  const base = Airtable.base(AIRTABLE_BASE_ID);
+  // formName directs data to correct base
+  const { formName } = data;
+  // discard unnecessary form data
+  delete data.formName;
+  delete data.recaptcha;
+  // send form data
+  base(formName).create(data, err => {
+    if (err) throw new Error(err);
   });
+};
+const verifyRecaptcha = async recaptcha => {
+  const secret = RECAPTCHA_SECRET;
+  const response = await axios.post(
+    `https://www.google.com/recaptcha/api/siteverify?secret=${secret}&response=${recaptcha}`,
+    {},
+  );
+
+  if (!response.data.success) {
+    throw new Error(response.data['error-codes']);
+  }
+  return response.data;
+};
 
 module.exports = async function handler(event) {
   try {
