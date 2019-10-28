f3b3b7b6495ebef85aaf3228bdd8dbf9cc6bb095
mn <39255541+margielamadman@users.noreply.github.com>
2019-10-28 16:44:50 -0700

    Remove airtable from formhandler

--- static/server/formHandler.js
+++ static/server/formHandler.js
@@ -1,27 +1,19 @@
 require('dotenv').config();
-const Airtable = require('airtable');
 const axios = require('axios');
 
-const {
-  GATSBY_AIRTABLE_API_KEY,
-  AIRTABLE_BASE_ID,
-  RECAPTCHA_SECRET,
-} = process.env;
+const { RECAPTCHA_SECRET } = process.env;
 
 const saveContact = async data => {
-  Airtable.configure({
-    apiKey: GATSBY_AIRTABLE_API_KEY,
-  });
-  const base = Airtable.base(AIRTABLE_BASE_ID);
   // formName directs data to correct base
   const { formName } = data;
   // discard unnecessary form data
   delete data.formName;
   delete data.recaptcha;
   // send form data
-  base(formName).create(data, err => {
-    if (err) throw new Error(err);
-  });
+  // TODO: POST data to our db
+  // base(formName).create(data, err => {
+  //   if (err) throw new Error(err);
+  // });
 };
 const verifyRecaptcha = async recaptcha => {
   const secret = RECAPTCHA_SECRET;
