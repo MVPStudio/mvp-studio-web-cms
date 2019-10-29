00d7144e474becf7494af3185814e063e503e9dc
mn <39255541+margielamadman@users.noreply.github.com>
2019-10-29 15:48:01 -0700

    Remove /api

--- api/src/Server.ts
+++ api/src/Server.ts
@@ -8,31 +8,31 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.get('/api', (req: Request, res: Response) => {
+app.get('/', (req: Request, res: Response) => {
     res.sendStatus(200);
 });
 
-app.get('/api/project/:projectID', (req: Request, res: Response) => {
+app.get('/project/:projectID', (req: Request, res: Response) => {
     res.send('Return project info');
 });
 
-app.post('/api/project', (req: Request, res: Response) => {
+app.post('/project', (req: Request, res: Response) => {
     res.send('Add a new project to the database');
 });
 
-app.put('/api/project/:projectID', (req: Request, res: Response) => {
+app.put('/project/:projectID', (req: Request, res: Response) => {
     res.send('Update and existing project');
 });
 
-app.delete('/api/project/:projectID', (req: Request, res: Response) => {
+app.delete('/project/:projectID', (req: Request, res: Response) => {
     res.send('Delete and existing project');
 });
 
-app.get('/api/projects', (req: Request, res: Response) => {
+app.get('/projects', (req: Request, res: Response) => {
     res.send('Return list of all projects');
 });
 
-app.post('/api/volunteer', (req: Request, res: Response) => {
+app.post('/volunteer', (req: Request, res: Response) => {
     res.send('Send volunteer info to project owner');
 });
 
