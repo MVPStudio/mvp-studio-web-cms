26fe403b93dc285ea6734e6da0190d76d3849f76
Miguel <39255541+margielamadman@users.noreply.github.com>
2019-11-12 12:09:43 +0000

    Update start file to create the db and start the server.

--- api/src/start.ts
+++ api/src/start.ts
@@ -2,11 +2,24 @@ import app from './Server';
 import { config } from './config';
 import { logger } from './logger';
 import { getDbClientInstance } from './database/dbClient';
+import { createDatabase } from './database/createDatabase';
 
-// Database client
-const dbClient = getDbClientInstance();
-// Start the server
-const port = config.serverPort;
-app.listen(port, () => {
-    logger.info('Express server started on port: ' + port);
-});
+export const runServer = async () => {
+    // Create database
+    await createDatabase();
+    // Database client
+    const dbClient = getDbClientInstance();
+
+    // Start the server
+    const port = config.serverPort;
+    await new Promise((resolve) => {
+        app.listen(port, () => {
+            logger.info('Express server started on port: ' + port);
+            resolve();
+        });
+    });
+};
+
+if (require.main === module) {
+    runServer();
+}
