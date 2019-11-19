7f2679763c910f2c0cee87230f1f86df852d9c2f
mn <39255541+margielamadman@users.noreply.github.com>
2019-11-19 05:42:51 -0800

    Not tolerating empty env values

--- api/src/config.ts
+++ api/src/config.ts
@@ -1,16 +1,31 @@
-const getEnvString = (name: string): string => process.env[name] || '';
-const getEnvNumber = (name: string): number => parseInt(getEnvString(name), 10);
-const getEnvBoolean = (name: string): boolean => getEnvString(name) === 'true';
+const getEnvString = (name: string, defaultValue: string): string => {
+    let value = '';
+    try {
+        if(!(process.env[name]) && defaultValue === '') {
+            throw new Error('Environment variable is undefined');
+        } else if(!(process.env[name])) {
+            value = defaultValue;
+        } else {
+            value = <string> process.env[name];
+        }
+    }
+    catch(e) {
+        console.log(e);
+    }
+    return value;
+}
+const getEnvNumber = (name: string, defaultValue: string): number => parseInt(getEnvString(name, defaultValue), 10);
+const getEnvBoolean = (name: string, defaultValue: string): boolean => getEnvString(name, defaultValue) === 'true';
 
 export const config = {
-    serverPort: getEnvNumber('SERVER_PORT') || 8000,
+    serverPort: getEnvNumber('SERVER_PORT', '8000'),
     database: {
         client: 'pg',
         connection: {
-            host: getEnvString('DATABASE_HOST'),
-            user: getEnvString('DATABASE_USER'),
-            password: getEnvString('DATABASE_PASSWORD'),
-            database: getEnvString('DATABASE_NAME'),
+            host: getEnvString('DATABASE_HOST', 'localhost'),
+            user: getEnvString('DATABASE_USER', 'postgres'),
+            password: getEnvString('DATABASE_PASSWORD', 'password'),
+            database: getEnvString('DATABASE_NAME', 'db'),
         },
     },
 };
