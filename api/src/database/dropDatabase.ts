223f57c29ba8e70088c7aba25b6cb06a40e28e67
Miguel <39255541+margielamadman@users.noreply.github.com>
2019-11-26 23:21:50 +0000

    Linting

--- api/src/database/dropDatabase.ts
+++ api/src/database/dropDatabase.ts
@@ -4,8 +4,8 @@ import { destroyClientAndExit, setupConnectionConfig, setupDbClient } from './se
 export const dropDatabase = async () => {
     logger.info(`Dropping database ${setupConnectionConfig.database}.`);
     try {
-        await setupDbClient.raw(`DROP DATABASE ${setupConnectionConfig.database}`)
-        logger.info('Database dropped.')
+        await setupDbClient.raw(`DROP DATABASE ${setupConnectionConfig.database}`);
+        logger.info('Database dropped.');
     } catch (e) {
             if (e.message.indexOf('does not exist') === -1) {
                 logger.error('Could not drop database.', e);
