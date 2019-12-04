d4f61b0051d8441bf0a313610b169b874e0a4c3a
Miguel <39255541+margielamadman@users.noreply.github.com>
2019-12-04 00:30:18 +0000

    Create 50 random projects

--- api/src/database/seeds/01_projects.ts.ts
+++ api/src/database/seeds/01_projects.ts.ts
@@ -1,38 +1,30 @@
 import * as Knex from 'knex';
+import faker from 'faker';
+
+const getRandomInt = (max: number) => {
+  return Math.floor(Math.random() * Math.floor(max));
+};
+
+const createFakeProject = () => ({
+    project_name: faker.company.companyName(),
+    org_url: faker.internet.url(),
+    po_name: faker.name.findName(),
+    po_email: faker.internet.email(),
+    description: faker.lorem.paragraph(),
+    description_link: faker.internet.url(),
+    status: getRandomInt(3),
+});
 
 export async function seed(knex: Knex): Promise<any> {
     // Deletes ALL existing entries
     return knex('projects').del()
         .then(() => {
+            const fakeProjects = [];
+            const projectPoolSize = 50;
+            for (let i = 0; i < projectPoolSize; i++) {
+                fakeProjects.push(createFakeProject());
+            }
             // Inserts seed entries
-            return knex('projects').insert([
-                { id: 1,
-                  project_name: 'Emerald Citizen',
-                  org_url: 'https://emeraldcitizen.org/',
-                  po_name: 'Scott Lively',
-                  po_email: 'scott@mvpstudio.org',
-                  description: 'Scott\'s cool open source React Magic',
-                  description_link: 'https://www.us-ignite.org/apps/emerald-citizen/',
-                  status: 1,
-                 },
-                { id: 2,
-                  project_name: 'MVP Studio',
-                  org_url: 'https://mvpstudio.org/',
-                  po_name: 'Scott Lively',
-                  po_email: 'scott@mvpstudio.org',
-                  description: 'We help visionaries build their vision',
-                  description_link: 'https://mvpstudio.org/',
-                  status: 1,
-                },
-                { id: 3,
-                  project_name: 'Test Project',
-                  org_url: 'https://google.org/',
-                  po_name: 'Test Name',
-                  po_email: 'test@test.org',
-                  description: 'This is a test project description.',
-                  description_link: 'https://google.org/',
-                  status: 0,
-                },
-            ]);
+            return knex('projects').insert(fakeProjects);
         });
 }
