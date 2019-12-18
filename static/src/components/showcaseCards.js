41d94b727436c273e470a86e4c422c889c39f445
miguel <39255541+margielamadman@users.noreply.github.com>
2019-12-18 16:47:25 +0000

    Add development proxy

--- static/src/components/showcaseCards.js
+++ static/src/components/showcaseCards.js
@@ -37,7 +37,7 @@ const ShowcaseCards = () => {
   const [projects, setProjects] = useState([]);
   useEffect(() => {
     async function fetchData() {
-      const response = await fetch('http://localhost/api/projects');
+      const response = await fetch('/api/projects');
       setProjects(await response.json());
     }
     fetchData();
