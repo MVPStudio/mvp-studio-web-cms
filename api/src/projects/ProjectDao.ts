4dfe835917eacb431a59e0f37e7ed633738a46d5
miguel <39255541+margielamadman@users.noreply.github.com>
2020-01-07 01:53:34 +0000

    Use typescript constructor shorthand

--- api/src/projects/ProjectDao.ts
+++ api/src/projects/ProjectDao.ts
@@ -2,9 +2,6 @@ import Knex = require('knex');
 import { getDbClientInstance } from '../database/dbClient';
 
 export default class ProjectDao {
-    private db: Knex;
-    constructor(db = getDbClientInstance()) {
-       this.db = db;
-    }
+    constructor(private db: Knex) {}
     public getAllProjects = async () => await this.db.select().from('projects');
 }
--- api/src/projects/ProjectService.ts
+++ api/src/projects/ProjectService.ts
@@ -1,10 +1,7 @@
 import ProjectDao from './ProjectDao';
 
 export default class ProjectService {
-    private dao: ProjectDao;
-    constructor(dao = new ProjectDao()) {
-        this.dao = dao;
-    }
+    constructor(private dao: ProjectDao) {}
     public async getAllProjects() {
       return this.dao.getAllProjects();
     }
