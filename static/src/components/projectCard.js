2f5cf584d8b93499119ecd33744aac5c597c6759
miguel <39255541+margielamadman@users.noreply.github.com>
2020-01-14 23:31:20 +0000

    Project Card finished

--- static/src/components/projectCard.js
+++ static/src/components/projectCard.js
@@ -1,4 +1,6 @@
 import React, {useState, useEffect} from 'react';
+import { Link } from 'gatsby';
+import { Line } from '../utilities';
 import styled from 'styled-components';
 
 const ProjectCard = styled.div`
@@ -18,19 +20,33 @@ const ProjectCard = styled.div`
   }
 `;
 
+const getProjectCard = ({project_name, org_url, po_name, description, description_link}) => (
+  <ProjectCard key={project_name}>
+    <div>
+      <h2>{project_name}</h2>
+      <Line />
+      <ul>
+        <li>Description: {description}</li>
+        <Link to={description_link}><p>Click here for a more detailed description.</p></Link>
+        <li>Team Members: {po_name}</li>
+        <li>Link: {org_url}</li>
+      </ul>
+    </div>
+  </ProjectCard>
+)
 
-const ProjectDetails = ({location}) => {
-//   const [project, setProject] = useState([]);
-//   useEffect(() => {
-//     async function fetchData() {
-//       const response = await fetch('/api/projects');
-//       setProjects(await response.json());
-//     }
-//     fetchData();
-//   }, [])
+const ProjectDetails = (props) => {
+  const [project, setProject] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(`/api/project/${props.projectID}`);
+      setProject(await response.json());
+    }
+    fetchData();
+  }, [])
   return (
       <div>
-        Hello
+        {project.map(getProjectCard)}
       </div>
   )
 };
