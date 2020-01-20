5630264343d08d9ec47c1d02643b19c1fdbedbbd
miguel <39255541+margielamadman@users.noreply.github.com>
2020-01-20 04:41:59 +0000

    Address changes requested in PR #86

--- static/src/components/projectCard.js
+++ static/src/components/projectCard.js
@@ -1,13 +1,9 @@
 import React, {useState, useEffect} from 'react';
-import { Link } from 'gatsby';
 import { Line } from '../utilities';
 import styled from 'styled-components';
 
 const ProjectCard = styled.div`
   margin: 0.5rem auto;
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minMax(200px, auto));
-  grid-gap: 1rem;
   align-items: center;
   a {
     text-align: center;
@@ -20,22 +16,28 @@ const ProjectCard = styled.div`
   }
 `;
 
-const getProjectCard = ({project_name, org_url, po_name, description, description_link}) => (
-  <ProjectCard key={project_name}>
-    <div>
-      <h2>{project_name}</h2>
-      <Line />
-      <ul>
-        <li>Description: {description} <a href={description_link}>Click here for a more detailed description.</a></li>
-        <li>Team Members: {po_name}</li>
-        <li>Link: {org_url}</li>
-      </ul>
-    </div>
-  </ProjectCard>
-)
+const getProjectCard = ({project_name, org_url, po_name, description, description_link}) => {
+  if (project_name) {
+    // Project exists
+    return (
+      <ProjectCard key={project_name}>
+          <h2>{project_name}</h2>
+          <Line />
+          <ul>
+            <li>Description: {description} <a href={description_link}>Click here for a more detailed description.</a></li>
+            <li>Team Members: {po_name}</li>
+            <li>Link: {org_url}</li>
+          </ul>
+      </ProjectCard>
+    );
+  } else {
+    // Project does not exist
+    return (<h3>Sorry, this project does not exist.</h3>);
+  }
+}
 
 const ProjectDetails = (props) => {
-  const [project, setProject] = useState([]);
+  const [project, setProject] = useState({});
   useEffect(() => {
     async function fetchData() {
       const response = await fetch(`/api/project/${props.projectID}`);
@@ -45,7 +47,7 @@ const ProjectDetails = (props) => {
   }, [])
   return (
       <div>
-        {project.map(getProjectCard)}
+        {getProjectCard(project)}
       </div>
   )
 };
