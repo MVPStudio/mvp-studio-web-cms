6a84b3c45239757c33c198db024c35e5bb04c68d
Miguel <39255541+margielamadman@users.noreply.github.com>
2020-02-04 16:45:54 -0800

    Update project table (#88)
    * Initial volunteer form test
    
    * Work in progress
    
    * Show form on click
    
    * Running into error...
    
    * Center the gear icon
    
    * WIP: preparing frontend form submit
    
    * Add regex to check project id parameter
    
    * Update linter to not enforce interface-name rule
    
    * Add Volunteer interface and  service method
    
    * Frontend volunteer form submission working
    
    * Check if project exists, before showing anything
    
    * Passing project id to backend api via form
    
    * Add timestamp row to project table
    
    * Add logo url to project table
    
    Co-authored-by: Jase Pellerin <jasepellerin@gmail.com>

--- api/src/database/migrations/20191112124521_initial.ts
+++ api/src/database/migrations/20191112124521_initial.ts
@@ -7,12 +7,14 @@ const _MAX_DESCRIPTION_LENGTH: number = 512;
 export async function up(knex: Knex): Promise<any> {
     await knex.schema.createTable('projects', (table) => {
         table.increments();
+        table.timestamps(true, true); /* Adds created_at and updated_at columns to db, auto updates*/
         table.string('project_name', _MAX_NAME_LENGTH).notNullable();
         table.string('org_url', _MAX_URL_LENGTH);
         table.string('po_name', _MAX_NAME_LENGTH).notNullable();
         table.string('po_email', _MAX_NAME_LENGTH).notNullable();
         table.string('description', _MAX_DESCRIPTION_LENGTH).notNullable();
         table.string('description_link', _MAX_URL_LENGTH);
+        table.string('logo_link', _MAX_URL_LENGTH);
         table.integer('status').notNullable();
     });
 }
